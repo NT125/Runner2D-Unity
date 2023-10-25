e592b9a0292236be0caaa64bef97d5b273cbe027
Ismael Ignacio Miguel Torres <ignacio25.it@gmail.com>
2023-10-25 12:48:37 -0300

    Movimiento del nivel 1 configurado con Corrutinas

--- Assets/Scripts/StageMovement.cs
+++ Assets/Scripts/StageMovement.cs
@@ -11,11 +11,22 @@ public class StageMovement : MonoBehaviour
     void Start()
     {
         stageRB = GetComponent<Rigidbody2D>();
+        StartCoroutine(MoveStage());
     }
 
     // Update is called once per frame
     void Update()
     {
+    }
+
+    /* void MoveStage(){
         stageRB.transform.Translate(Vector2.left * stageSpeed * Time.deltaTime);
+    } */
+
+    IEnumerator MoveStage(){
+        while(true){
+            stageRB.transform.Translate(Vector2.left * stageSpeed * Time.deltaTime);
+            yield return null;
+        }
     }
 }
