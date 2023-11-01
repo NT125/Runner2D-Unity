427418468f75d3946c7ce15b3d44045aac6c4733
Ismael Ignacio Miguel Torres <ignacio25.it@gmail.com>
2023-11-01 15:00:19 -0300

    Implementación del segundo enemigo finalizada, con alerta previa

--- Assets/Scripts/SpikeEnemyTrigger.cs
+++ Assets/Scripts/SpikeEnemyTrigger.cs
@@ -1,16 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class SpikeEnemyTrigger : MonoBehaviour
 {
     [SerializeField] private GameObject spikeEnemy;
+    [SerializeField] private GameObject alert;
     [SerializeField] private float offset = 2f;
 
     void OnTriggerEnter2D(Collider2D col)
     {
         if (col.CompareTag("Player"))
         {
+            StartCoroutine(LaunchAlert());
             Debug.Log("Hay colisión");
             //Vector2 spawnLocation = transform.position + Vector3.left * offset; // Creando un punto de spawn, "offset" unidades a la derecha
             Invoke("SpawnEnemy", 0.7f);
@@ -19,5 +22,11 @@ public class SpikeEnemyTrigger : MonoBehaviour
 
     private void SpawnEnemy(){
         Instantiate(spikeEnemy, transform.position + Vector3.left * offset, Quaternion.identity);
+    }
+
+    IEnumerator LaunchAlert(){
+        GameObject newAlert = Instantiate(alert, new Vector2(-7f, -1.5f), Quaternion.identity);
+        yield return new WaitForSeconds(0.7f);
+        Destroy(newAlert);
     }
 }
